39d00706f744276c7f808a0cd853a53dc620872c
Alexia <alexheg44@gmail.com>
2021-04-14 20:47:17 -0400

    changed api

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,9 @@
 class API 
-    def self.get_data(input)
-    response = RestClient.get('https://www.freetogame.com/api/games?category=mmorpg')
+    def self.get_data
+    response = RestClient.get('https://www.freetogame.com/api/games?tag=fantasy&category=mmorpg')
     game_list = JSON.parse(response)
     game_list.each do |game|
-        Game.new(game)
+        Games.new(game)
     end
     #binding.pry
 end
